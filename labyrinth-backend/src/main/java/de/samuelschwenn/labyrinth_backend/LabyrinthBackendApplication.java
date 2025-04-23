e1b5fd92a3b3f64d1dc0ce77c1a9fdfc98ad4b4b
Schwenn, Samuel <Samuel.Schwenn@adesso.de>
2025-04-23 11:24:51 +0200

    Initial Commit

---
+++ labyrinth-backend/src/main/java/de/samuelschwenn/labyrinth_backend/LabyrinthBackendApplication.java
@@ -0,0 +1,13 @@
+package de.samuelschwenn.labyrinth_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabyrinthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabyrinthBackendApplication.class, args);
+	}
+
+}
